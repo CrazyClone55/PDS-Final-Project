56f7b9e31a647527d3588d424fcc600b802efabe
Shaan Patel <shaan@webitup.com>
2022-04-01 10:23:43 -0500

    Deploy to Heroku

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, "../client/build")));
+
+app.get("/api", (req, res) => {
+  console.log(`sent message`);
+  res.json({ message: "hello from server!" });
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
